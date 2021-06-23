4bc1085578d1f0789a1b6a0cc5e4cc45eba4e191
ruhulamin01749 <ruhul11bd@gmail.com>
2021-06-24 02:47:20 +0600

    Drawer navigation setup

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
   View,
 } from 'react-native';
 
+import { Icon, List } from 'native-base';
+
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
@@ -23,7 +25,7 @@ import Category from './pages/Category'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { themeColor } from './helper/AppHelper';
+import { textColor, themeColor } from './helper/AppHelper';
 import DrawerContent from './navigation/DrawerContent';
 
 const Stack = createStackNavigator();
@@ -48,12 +50,15 @@ const App= () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>        
-        <Drawer.Screen name="Home" component={stackNav} options={{}} />
-        <Drawer.Screen name="Category" component={Category} />
+        <Drawer.Screen name="Home" component={stackNav} options={{ drawerIcon: (({focused}) => <Icon name="home" style={{color: textColor, marginRight:-15, fontSize:20}} />) }} />
+        <Drawer.Screen name="Watch History" component={List} options={{ drawerIcon: (({focused}) => <Icon name="stopwatch-outline" style={{color: textColor, marginRight:-15, fontSize:20}} />) }} />
+        <Drawer.Screen name="My Favorite" component={List} options={{ drawerIcon: (({focused}) => <Icon name="ios-heart-circle-outline" style={{color: textColor, marginRight:-15, fontSize:20}} />) }} />
+        <Drawer.Screen name="Network Stream" component={List} options={{ drawerIcon: (({focused}) => <Icon name="logo-playstation" style={{color: textColor, marginRight:-15, fontSize:20}} />) }} />
+        {/* <Drawer.Screen name="Category" component={Category} />
         <Drawer.Screen name="Search" component={Search} />
         <Drawer.Screen name="Movies" component={ListPage} />
         <Drawer.Screen name="TV-Series" component={ListPage} />
-        <Drawer.Screen name="Web Series" component={ListPage} />
+        <Drawer.Screen name="Web Series" component={ListPage} /> */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -10,8 +10,9 @@ import {
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
-import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { themeColor, bodyColor, textColor } from '../helper/AppHelper';
+import { Icon } from 'native-base';
 
 
 
@@ -23,8 +24,84 @@ export default function DrawerContent(props){
                     <View style={{alignItems:'center'}}>
                         <Image style={styles.logo} source={require('../assets/img/logo.jpg')} />
                     </View>
-                    <View style={{padding:15, marginTop:15}}>
-                        <Text style={{fontSize:14, color:textColor}}> App Language</Text>
+                    <View style={{padding:0, marginTop:15}}>
+                        {/* <Text style={{fontSize:14, color:textColor, paddingLeft:17, fontSize:16}}>
+                            <Icon color={'red'} size={16} name={'heart-outline'} />  App Language
+                        </Text> */}
+                        
+                        {/* Drawer item list */}
+                        <DrawerItemList {...props}
+                            labelStyle={{color: textColor, fontSize:15}}
+                        />
+                        
+                        <Text style={{marginTop:10, marginBottom:-10, borderTopWidth:1, borderTopColor: themeColor }}></Text>
+
+                        <DrawerItem
+                            label="Movies"
+                            icon={({ focused, color, size }) => <Icon name={'film'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('list')}
+                        />
+                        <DrawerItem
+                            label="Category"
+                            icon={({ focused, color, size }) => <Icon name={'pricetag'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('category')}
+                        />
+
+                        <DrawerItem
+                            label="Search"
+                            icon={({ focused, color, size }) => <Icon name={'search'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('search')}
+                        />
+
+                        <Text style={{marginTop:10, marginBottom:-10, borderTopWidth:1, borderTopColor: themeColor }}></Text>
+
+                        <DrawerItem
+                            label="Web Series"
+                            icon={({ focused, color, size }) => <Icon name={'ios-film-outline'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('list')}
+                        />
+                        <DrawerItem
+                            label="Category"
+                            icon={({ focused, color, size }) => <Icon name={'pricetag'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('category')}
+                        />
+
+                        <DrawerItem
+                            label="Search"
+                            icon={({ focused, color, size }) => <Icon name={'search'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('search')}
+                        />
+
+                        <Text style={{marginTop:10, marginBottom:-10, borderTopWidth:1, borderTopColor: themeColor }}></Text>
+
+                        <DrawerItem
+                            label="TV Series"
+                            icon={({ focused, color, size }) => <Icon name={'tv-outline'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('list')}
+                        />
+                        <DrawerItem
+                            label="Category"
+                            icon={({ focused, color, size }) => <Icon name={'pricetag'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('category')}
+                        />
+
+                        <DrawerItem
+                            label="Search"
+                            icon={({ focused, color, size }) => <Icon name={'search'} style={{fontSize:18, color:textColor}} />}
+                            labelStyle={{color:textColor, fontSize:16, marginLeft:-15}}
+                            onPress={()=>navigation.navigate('search')}
+                        />
+                        
+
+
                     </View>
                 </View>
             </DrawerContentScrollView>
